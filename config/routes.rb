017366e565d27aae3f0d843e8ff6247719497291
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-05-31 20:12:30 +0000

    [Add] 初期データの作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
       resource :favorite, only: [:create, :destroy]
       resources :post_comments, only: [:create, :destroy]
     end
-    resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
   end
   
   
