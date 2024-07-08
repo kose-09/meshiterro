773791548ff51f751ba0f68cd1f94d1a39b0ee9e
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-07-08 17:43:15 +0000

    [Update] 各モデルに通知モデルとの関連付けを追記

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,7 @@ class Favorite < ApplicationRecord
 
   belongs_to :user
   belongs_to :post_image
+  has_one :notification, as: :notifiable, dependent: :destroy
 
   validates :user_id, uniqueness: {scope: :post_image_id}
 
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,8 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
+  
 
   validates :shop_name, presence: true
   validates :image, presence: true
