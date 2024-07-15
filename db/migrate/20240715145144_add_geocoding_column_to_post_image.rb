6b32b4cb855a3e0e443171c764d822fb6113a39a
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-07-15 17:29:27 +0000

    [Add] 新規投稿フォームに郵便番号と住所の入力欄を作成

--- db/migrate/20240715145144_add_geocoding_column_to_post_image.rb
+++ db/migrate/20240715145144_add_geocoding_column_to_post_image.rb
@@ -2,6 +2,6 @@ class AddGeocodingColumnToPostImage < ActiveRecord::Migration[6.1]
   def change
     add_column :post_images, :address, :string, null: false, default: ""
     add_column :post_images, :latitude, :float, null: false, default: 0
-    add_column :post_images, :longitude, :float, null: false, default: 
+    add_column :post_images, :longitude, :float, null: false, default: 0
   end
 end
