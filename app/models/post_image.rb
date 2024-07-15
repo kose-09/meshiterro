ca9c742e26d226e515b83931f3badd0374d4009d
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-07-15 15:16:50 +0000

    [Add] PostImageモデルにgeocodingの処理を追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -10,6 +10,10 @@ class PostImage < ApplicationRecord
 
   validates :shop_name, presence: true
   validates :image, presence: true
+  validates :address, presence: true
+  
+  geocoded_by :address
+  after_validation :geocode
   
   after_create do
     user.followers.each do |follower|
